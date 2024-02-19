2bdb27424dd7c5024f14900ce02b2f663ec6868d
Elijah Mari <[emari1@students.towson.edu]>
2024-02-19 12:23:13 -0500

    bob

---
+++ SingleValue.java
@@ -0,0 +1,9 @@
+public class SingleValue {
+    private int value;
+    public SingleValue(int otherValue){
+        value=otherValue;
+    }
+    public int getX(){
+        return value;
+    }
+}
